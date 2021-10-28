f69862a9f4a1b77c5c10679a36a530881552ca46
Pio <pio.cho.bia@gmail.com>
2021-10-28 09:01:13 +0200

    zmieniony sposób wpisywania wartości, startowe

--- ProjektPWF/Models/Obsluga.cs
+++ ProjektPWF/Models/Obsluga.cs
@@ -4,11 +4,86 @@ namespace ProjektPWF.Models
 {
     public class Obsluga
     {
-        //nawet specjalnie daje to
-        [Key]
-        public int Id { get; set; }
 
-        [StringLength(100)]
-        public string Stawka { get; set; }
+
+        public void  WpiszTabDostarczanie(TabDostarczanie t)
+        {     
+            using (var dbContext = new ApplicationDbContext())
+            {
+                dbContext.DostarczanieC.Add(t);
+                dbContext.SaveChanges();
+            }
+        }
+
+
+        public void  WpiszTabKierownik(TabKierownik t)
+        {     
+            using (var dbContext = new ApplicationDbContext())
+            {
+                dbContext.KierownikC.Add(t);
+                dbContext.SaveChanges();
+            }
+        }
+
+
+        public void  WpiszTabNabywca(TabNabywca t)
+        {     
+            using (var dbContext = new ApplicationDbContext())
+            {
+                dbContext.NabywcaC.Add(t);
+                dbContext.SaveChanges();
+            }
+        }
+
+
+        public void  WpiszTabPlatnosc(TabPlatnosc t)
+        {     
+            using (var dbContext = new ApplicationDbContext())
+            {
+                dbContext.PlatnoscC.Add(t);
+                dbContext.SaveChanges();
+            }
+        }
+
+
+        public void  WpiszTabSprzedaz(TabSprzedaz t)
+        {     
+            using (var dbContext = new ApplicationDbContext())
+            {
+                dbContext.SprzedazC.Add(t);
+                dbContext.SaveChanges();
+            }
+        }  
+     
+
+        public void  WpiszTabStatus(TabStatus t)
+        {     
+            using (var dbContext = new ApplicationDbContext())
+            {
+                dbContext.StatusC.Add(t);
+                dbContext.SaveChanges();
+            }
+        }
+
+
+        public void  WpiszTabTermin(TabTermin t)
+        {     
+            using (var dbContext = new ApplicationDbContext())
+            {
+                dbContext.TerminC.Add(t);
+                dbContext.SaveChanges();
+            }
+        }
+
+
+        public void  WpiszTabVat(TabVat t)
+        {     
+            using (var dbContext = new ApplicationDbContext())
+            {
+                dbContext.VatC.Add(t);
+                dbContext.SaveChanges();
+            }
+        }       
+
     }
 }
--- ProjektPWF/StartWindow.cs
+++ ProjektPWF/StartWindow.cs
@@ -15,78 +15,56 @@ namespace ProjektPWF
     public partial class StartWindow : Form
     {
 
-
-        public static void TabDostarczanie()
-        {
-            var dostarczanieL = new List<TabDostarczanie>
-            {
-                new TabDostarczanie{Sposob="list polecony"},
-                new TabDostarczanie{Sposob="e-mail"},
-                new TabDostarczanie{Sposob="osobiście"},
-                new TabDostarczanie{Sposob="kurier"}
-            };        
-            using (var dbContext = new ApplicationDbContext())
-            {
-                dbContext.DostarczanieC.AddRange(dostarczanieL);
-                dbContext.SaveChanges();
-            }
-        }
-
-
-        public static void StartoweTabKierownik()
-        {
-            var kierownikL = new List<TabKierownik>
-            {
-                new TabKierownik{ImieKierownika="Jan", NazwiskoKierownika="Kowalski"},
-                new TabKierownik{ImieKierownika="Adam", NazwiskoKierownika="Nowak"},
-                new TabKierownik{ImieKierownika="Michał", NazwiskoKierownika="Mały"},
-            };        
-            using (var dbContext = new ApplicationDbContext())
-            {
-                dbContext.KierownikC.AddRange(kierownikL);
-                dbContext.SaveChanges();
-            }
-        }
-
-
-        public static void StartoweTabNabywca()
-        {
-            var list = new List<TabNabywca>
-            {
-                new TabNabywca{ NazwaNabywcy="Słoneczny Wschód",  AdresNabywcy="Soso ul Wschodnia 3", NIP="1123456789"},
-                new TabNabywca{ NazwaNabywcy="Słoneczne Południe",  AdresNabywcy="Wrocław ul Południowa 13", NIP="2123456789"},
-                new TabNabywca{ NazwaNabywcy="Słoneczny Zachód",  AdresNabywcy="Gdynia ul Zachodnia 113", NIP="3123456789"},
-            };        
-            using (var dbContext = new ApplicationDbContext())
-            {
-                dbContext.NabywcaC.AddRange(list);
-                dbContext.SaveChanges();
-            }
-        }
-
-
-        public static void StartoweTabPlatnosc()
-        {
-            var list = new List<TabPlatnosc>
-            {
-                new TabPlatnosc{ Rodzaj="przelew"},
-                new TabPlatnosc{ Rodzaj="za pobraniem"}
-            };        
-            using (var dbContext = new ApplicationDbContext())
-            {
-                dbContext.PlatnoscC.AddRange(list);
-                dbContext.SaveChanges();
-            }
-        }
-
-
-        public static void DopisTabStatus(TabStatus o1)
+        public static void WpiszStartoweDoTabel()
         {     
-            using (var dbContext = new ApplicationDbContext())
-            {
-                dbContext.StatusC.Add(o1);
-                dbContext.SaveChanges();
-            }
+            var obsluga = new Obsluga();
+           
+            /*
+            //tabela DostarczanieC
+            obsluga.WpiszTabDostarczanie( new TabDostarczanie{ Sposob="list polecony" } );
+            obsluga.WpiszTabDostarczanie( new TabDostarczanie{ Sposob="e-mail" } );
+            obsluga.WpiszTabDostarczanie( new TabDostarczanie{ Sposob="osobiście" } );
+            obsluga.WpiszTabDostarczanie( new TabDostarczanie{ Sposob="kurier" } );
+
+            
+            //tabela KierownikC
+            obsluga.WpiszTabKierownik( new TabKierownik{ ImieKierownika="Jan", NazwiskoKierownika="Kowalski" } );
+            obsluga.WpiszTabKierownik( new TabKierownik{ ImieKierownika="Adam", NazwiskoKierownika="Nowak" } );
+            obsluga.WpiszTabKierownik( new TabKierownik{ ImieKierownika="Michał", NazwiskoKierownika="Mały" } );
+
+            
+            //tabela NabywcaC
+            obsluga.WpiszTabNabywca( new TabNabywca{ NazwaNabywcy="Słoneczny Wschód",  AdresNabywcy="Soso ul Wschodnia 3", NIP="1123456789" } );
+            obsluga.WpiszTabNabywca( new TabNabywca{ NazwaNabywcy="Słoneczne Południe",  AdresNabywcy="Wrocław ul Południowa 13", NIP="2123456789" } );
+            obsluga.WpiszTabNabywca( new TabNabywca{ NazwaNabywcy="Słoneczny Zachód",  AdresNabywcy="Gdynia ul Zachodnia 113", NIP="3123456789" } );
+
+           
+            //tabela PlatnoscC
+            obsluga.WpiszTabPlatnosc( new TabPlatnosc{ Rodzaj="przelew" } );
+            obsluga.WpiszTabPlatnosc( new TabPlatnosc{ Rodzaj="za pobraniem" } );
+
+            
+            //tabela StatusC
+            obsluga.WpiszTabStatus(new TabStatus{ Status="nowa" } );
+            obsluga.WpiszTabStatus(new TabStatus{ Status="anulowana" } );
+            obsluga.WpiszTabStatus(new TabStatus{ Status="częściowo wystawiona ( cykliczna )" } );
+            obsluga.WpiszTabStatus(new TabStatus{ Status="wystawiona" } );
+           
+
+            //tabela TerminC
+            obsluga.WpiszTabTermin( new TabTermin{ Termin=7 } );
+            obsluga.WpiszTabTermin( new TabTermin{ Termin=14 } );
+            obsluga.WpiszTabTermin( new TabTermin{ Termin=21 } );
+            obsluga.WpiszTabTermin( new TabTermin{ Termin=30 } );
+
+
+            //tabela VatC
+            obsluga.WpiszTabVat( new TabVat{ Stawka="00" } );
+            obsluga.WpiszTabVat( new TabVat{ Stawka="ZW" } );
+            obsluga.WpiszTabVat( new TabVat{ Stawka="5 %" } );
+            obsluga.WpiszTabVat( new TabVat{ Stawka="8 %" } );
+            obsluga.WpiszTabVat( new TabVat{ Stawka="23 %" } );
+            */
         }
 
 
@@ -98,17 +76,7 @@ namespace ProjektPWF
         {
             InitializeComponent();
 
-            //tworzenie przykladowych rekordów i zapis do tabel     
-            //StartoweTabDostarczanie();
-            //StartoweTabKierownik();
-            //StartoweTabNabywca();
-            //StartoweTabPlatnosc();
-           // DopisTabStatus(new TabStatus { Status="nowa"});
-
-            var xx = new TabVat();
-           var obsluga = new Obsluga();
-       
-
+            WpiszStartoweDoTabel();
 
 
             /*
